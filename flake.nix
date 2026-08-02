9e102a57b5ff30351080f2a8e78bf94431209d9b
Lyre <yvan.fedaouche@gmail.com>
2026-08-02 02:17:19 +0200

    feat(plasma-manager)

--- flake.nix
+++ flake.nix
@@ -8,6 +8,11 @@
             url = "github:nix-community/home-manager/release-26.05";
             inputs.nixpkgs.follows = "nixpkgs";
         };
+        plasma-manager = {
+            url = "github:nix-community/plasma-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+            inputs.home-manager.follows = "home-manager";
+        };
     };
 
     outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs: {
@@ -23,6 +28,7 @@
                         home-manager.useGlobalPkgs = true;
                         home-manager.useUserPackages = true;
                         home-manager.extraSpecialArgs = { inherit inputs; };
+                        home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
                         home-manager.users.lyre = import ./home-manager/home.nix;
                     }
                 ];
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -25,5 +25,51 @@
      };
   };
 
+  programs.plasma = {
+    enable = true;
+
+    configFile = {
+      "kwinrc" = {
+        "Desktops" = {
+          "Number" = 10;
+        };
+      };
+    };
+
+    shortcuts = {
+      "kwin" = {
+        "Switch to Desktop 1" = "Meta+&";
+        "Window to Desktop 1" = "Meta+1";
+
+        "Switch to Desktop 2" = "Meta+é";
+        "Window to Desktop 2" = "Meta+2";
+
+        "Switch to Desktop 3" = "Meta+\"";
+        "Window to Desktop 3" = "Meta+3";
+
+        "Switch to Desktop 4" = "Meta+'";
+        "Window to Desktop 4" = "Meta+4";
+
+        "Switch to Desktop 5" = "Meta+(";
+        "Window to Desktop 5" = "Meta+5";
+
+        "Switch to Desktop 6" = "Meta+-";
+        "Window to Desktop 6" = "Meta+6";
+
+        "Switch to Desktop 7" = "Meta+è";
+        "Window to Desktop 7" = "Meta+7";
+
+        "Switch to Desktop 8" = "Meta+_";
+        "Window to Desktop 8" = "Meta+8";
+
+        "Switch to Desktop 9" = "Meta+ç";
+        "Window to Desktop 9" = "Meta+9";
+
+        "Switch to Desktop 10" = "Meta+à";
+        "Window to Desktop 10" = "Meta+0";
+      };
+    };
+  };
+
   programs.home-manager.enable = true;
 }
