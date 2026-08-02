f87f0274f8c36f812322a6ce2daae8518048c11d
Lyre <yvan.fedaouche@gmail.com>
2026-08-02 02:20:32 +0200

    fix(flake)

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
                         home-manager.useGlobalPkgs = true;
                         home-manager.useUserPackages = true;
                         home-manager.extraSpecialArgs = { inherit inputs; };
-                        home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
+                        home-manager.sharedModules = [ plasma-manager.homeModules.plasma-manager ];
                         home-manager.users.lyre = import ./home-manager/home.nix;
                     }
                 ];
